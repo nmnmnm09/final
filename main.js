223d450e8df2b7c1060741e9b1cd52f6545f9720
Leo <nmnmnm1230@gmail.com>
2019-06-07 14:42:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -240,7 +240,7 @@ allow drawing on canvas
 function allowDrawing() {
     canvas.isDrawingMode = 1;
     if (mode == 'en')
-        document.getElementById('status').innerHTML = 'Model Loaded';
+        document.getElementById('status').innerHTML = '載入完成';
     else
         document.getElementById('status').innerHTML = 'تم التحميل';
     $('button').prop('disabled', false);
